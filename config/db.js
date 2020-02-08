fe633be974439fac84561d1836adc2cfe2a1e8c8
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2020-02-08 17:24:40 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,29 +1,17 @@
-// var mongoose = require('mongoose');
-// // const uri = 'mongodb://localhost:27017/idTab';
-// const uri = 'mongodb+srv://ktsyr1:idlibtp123@cluster0-6xfyu.mongodb.net/test?retryWrites=true&w=majority';
+var mongoose = require('mongoose');
+const uri = 'mongodb://localhost:27017/idTab';
 
-// mongoose.createConnection(uri, {
+mongoose.connect(uri, {
 
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true
-// }, (err) => {
-//     if (err) {
-//         console.log(err);
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (err) {
+        console.log(err);
 
-//     } else {
-//         console.log('db connected');
+    } else {
+        console.log('db connected');
 
-//     }
-// });
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://ktsyr1:idlibtp123@cluster0-6xfyu.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true ,useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("idTab").collection("center");
-  // perform actions on the collection object
-  console.log("gjrrl");
-  
-  client.close();
+    }
 });
