5af24a37f7ef71023b0a9b4bf461626f22a6a138
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2020-02-08 23:52:30 +0200

    cghvjbkn

--- config/db.js
+++ config/db.js
@@ -1,17 +1,29 @@
-var mongoose = require('mongoose');
-const uri = 'mongodb://localhost:27017/idTab';
+// var mongoose = require('mongoose');
+// const uri = 'mongodb://localhost:27017/idTab';
 
-mongoose.connect(uri, {
+// mongoose.connect(uri, {
 
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-}, (err) => {
-    if (err) {
-        console.log(err);
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true
+// }, (err) => {
+//     if (err) {
+//         console.log(err);
 
-    } else {
-        console.log('db connected');
+//     } else {
+//         console.log('db connected');
 
-    }
+//     }
+// });
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://ktsyr1:idlibtp123@cluster0-6xfyu.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
+client.connect(err => {
+  const collection = client.db("id-Tab").collection("center");
+
+console.log("fhyjghiuoj;piufydtshrhtdyfukgilho;jo;higufyjd");
+
+  client.close();
 });
